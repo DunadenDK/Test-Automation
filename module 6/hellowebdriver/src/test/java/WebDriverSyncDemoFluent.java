9093e9016d1a3ae1ea834e23bb0802e6ba405efd
Denis Kamyshov <denis_kamyshov@epam.com>
2019-08-15 15:59:42 +0400

    More demo

--- module 6/hellowebdriver/src/test/java/WebDriverSyncDemoFluent.java
+++ module 6/hellowebdriver/src/test/java/WebDriverSyncDemoFluent.java
@@ -1,14 +1,19 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
-public class HelloWebDriver {
-
+public class WebDriverSyncDemoFluent {
     public static void main(String[] args)  {
         //no remote webDriver run during this demo
         WebDriver driver =  new ChromeDriver();
@@ -27,11 +32,20 @@ public class HelloWebDriver {
         searchBtn.get(0).click();
         //risky point here
 
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions
-                        .presenceOfAllElementsLocatedBy(By.xpath("//div[contains(@class,'gsc-webResult') and contains (.,'selenium') and contains(.,'java')]")));
 
-        List<WebElement> searchResults = driver.findElements(By.xpath("//div[contains(@class,'gsc-webResult') and contains (.,'selenium') and contains(.,'java')]"));
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .withMessage("Timeout for waiting search result list was exceeded!");
+
+        List<WebElement> searchResults = wait.until(new Function<WebDriver, List<WebElement>>() {
+            public List<WebElement> apply(WebDriver driver) {
+                return driver.findElements(By.xpath("//div[contains(@class,'gsc-webResult') and contains (.,'selenium') and contains(.,'java')]"));
+            }
+        });
+
         System.out.println("Search results number for requested term: " + searchResults.size());
 
         //Thread.sleep(5000);
@@ -41,7 +55,7 @@ public class HelloWebDriver {
 
     private static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
         return new WebDriverWait(driver, 10)
-                    .until(ExpectedConditions
-                            .presenceOfElementLocated(by));
+                .until(ExpectedConditions
+                        .presenceOfElementLocated(by));
     }
 }
