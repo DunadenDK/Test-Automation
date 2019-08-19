cbc0dfacf173ac6d9eb80ab4f46945e5ec9345fd
Denis Kamyshov <denis_kamyshov@epam.com>
2019-08-19 17:57:54 +0400

    JUnit Runner and TestSuite

---
+++ module 5/src/test/java/com/epam/cdp/tests/junit/TestSuite.java
@@ -0,0 +1,15 @@
+package com.epam.cdp.tests.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({CosCalTest.class, SinCalTest.class, CtgCalTest.class, DivCalTest.class, IsNegativeCalTest.class, IsPositiveCalTest.class,
+        MultCalTest.class, PowCalTest.class, SinCalTest.class, SqrtCalTest.class, SubCalTest.class, SumCalTest.class, TgCalTest.class})
+
+public class TestSuite {
+
+}
