11954f613ff08b96f7a0af0cc155c3b7384f3606
Denis Kamyshov <denis_kamyshov@epam.com>
2019-09-25 16:05:48 +0400

    driver

--- module 8/src/test/java/SetUpAndTearDown.java
+++ module 8/src/test/java/SetUpAndTearDown.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+
 
 public class SetUpAndTearDown {
 
@@ -10,6 +12,13 @@ public class SetUpAndTearDown {
     private static final String LOGIN_PAGE_URL = "https://mail.yandex.by";
     private static final String GOOGLE_CHROME_DRIVER_PATH = ".//src/main/resources/chromedriver.exe";
     private static final String CHROME_DRIVER_PROPERTY = "webdriver.chrome.driver";
+
+    @BeforeClass(alwaysRun = true)
+    @Parameters({"os", "browser", "url", "node"})
+    public void setUp(String os, String browser, String url, String node) throws MalformedURLException {
+        RemoteDriverTest remoteDriverTest = new RemoteDriverTest(os, browser, url, node);
+        driver = remoteDriverTest.getDriver();
+    }
 
     @BeforeMethod
     public void browserSetup() {
